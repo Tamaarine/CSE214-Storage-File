407b895548fa4ca5d604232ea874fb9f13c29b92
FreezyCold <irebootplaygt@gmail.com>
2019-12-26 14:11:23 -0500

    Final Update CSE 214 Signing Off

--- CSE214TestingSite/src/TestClass.java
+++ CSE214TestingSite/src/TestClass.java
@@ -8,4 +8,61 @@ public class TestClass
         s=str;
         v=value;
     }
+
+    public static void main(String [] args)
+    {
+        int keys[]={1,1,1,1,1,0};
+        print(keys);
+        
+        sort(keys);
+        
+        System.out.println("---------------");
+        
+        int a=5;
+        
+        int result=a++ +a+ ++a;
+        
+        System.out.println(result);
+        
+    }
+    public static void sort(int keys[])
+    {
+        int zeroCounter=0;
+        int oneCounter=0;
+        
+        for(int i=0;i<keys.length;i++)
+        {
+            if(keys[i]==0)
+            {
+                zeroCounter++;
+            }
+        }
+        
+        oneCounter=keys.length-zeroCounter;
+        
+        for(int i=0;i<keys.length;i++)
+        {
+            if(zeroCounter>0)
+            {
+                keys[i]=0;
+                zeroCounter--;
+            }
+            else
+            {
+                keys[i]=1;
+            }
+        }
+        
+        print(keys);
+    }
+    
+    public static void print(int keys[])
+    {
+        for(int i=0;i<keys.length;i++)
+        {
+            System.out.print(keys[i]+",");
+        }
+        
+        System.out.println("");
+    }
 }
